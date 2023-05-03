0162990656f91c81a14dedd7a995f783de45df13
apfdhk <132316838+apfdhk@users.noreply.github.com>
2023-05-03 14:19:08 +0900

    퀴제리아 5.2

--- src/main/java/no/answer/quizzeria/repository/MemberRepository.java
+++ src/main/java/no/answer/quizzeria/repository/MemberRepository.java
@@ -4,8 +4,6 @@ import no.answer.quizzeria.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long>
         , QuerydslPredicateExecutor<Member>
 {
