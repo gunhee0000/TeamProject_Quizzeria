d68e80883b8f2cea22f7a003c0298cd4437224b2
apfdhk <132316838+apfdhk@users.noreply.github.com>
2023-05-03 14:19:08 +0900

    퀴제리아 5.2

--- src/main/java/no/answer/quizzeria/dto/PageResultDTO.java
+++ src/main/java/no/answer/quizzeria/dto/PageResultDTO.java
@@ -22,7 +22,7 @@ public class PageResultDTO<DTO, EN> {
     private boolean prev, next;
     private List<Integer> pageList;
 
-    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn){
+    public PageResultDTO(Page<EN> result, Function<EN, DTO>fn){
         dtoList = result.stream().map(fn).collect(Collectors.toList());
 
         totalPage = result.getTotalPages();
