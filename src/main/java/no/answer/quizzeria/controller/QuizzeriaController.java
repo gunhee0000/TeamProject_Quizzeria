dfa393e9ead28ea8106a728a01891f1a1ba6b78e
apfdhk <132316838+apfdhk@users.noreply.github.com>
2023-05-03 14:06:18 +0900

    퀴제리아 5.2

--- src/main/java/no/answer/quizzeria/controller/QuizzeriaController.java
+++ src/main/java/no/answer/quizzeria/controller/QuizzeriaController.java
@@ -2,7 +2,10 @@ package no.answer.quizzeria.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import no.answer.quizzeria.dto.PageRequestDTO;
+import no.answer.quizzeria.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class QuizzeriaController {
+    private final BoardService boardService;
 
     @GetMapping("/main/Welcome")
     public void Main(){
@@ -20,5 +24,9 @@ public class QuizzeriaController {
     public void Home(){
         log.info("Quizzeria_Home In");
     }
-
+    @GetMapping("/board/Board_main")
+    public void Board(PageRequestDTO pageRequestDTO, Model model){
+        log.info("Quizzeia_Board In");
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
 }
