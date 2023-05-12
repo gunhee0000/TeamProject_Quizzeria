5c34211309958c96d210a58879024775efd853eb
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2023-05-12 15:34:22 +0900

    퀴즈 공사중

--- src/main/java/no/answer/quizzeria/controller/QuizController.java
+++ src/main/java/no/answer/quizzeria/controller/QuizController.java
@@ -2,7 +2,11 @@ package no.answer.quizzeria.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import no.answer.quizzeria.dto.PageRequestDTO;
+import no.answer.quizzeria.service.QuizListService;
+import no.answer.quizzeria.service.QuizService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/quiz/")
 @RequiredArgsConstructor
 public class QuizController {
+
+    private final QuizListService quizListService;
+
     @GetMapping("/quiz_main")
-    public void Quiz_main(){
+    public void Quiz_main(PageRequestDTO pageRequestDTO, Model model){
         log.info("Quiz_main In");
+        model.addAttribute("quizlist", quizListService.getList(pageRequestDTO));
     }
 
     @GetMapping("/quiz_view")
