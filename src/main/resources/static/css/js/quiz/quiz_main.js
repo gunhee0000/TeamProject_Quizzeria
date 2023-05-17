15c1b3ca0d92f9440f63d505748caf01721df99d
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2023-05-17 11:39:28 +0900

    dd

--- src/main/resources/static/css/js/quiz/quiz_main.js
+++ src/main/resources/static/css/js/quiz/quiz_main.js
@@ -1,5 +1,5 @@
 function randomQuiz(){
     var quizSize = document.getElementById("size").value; //안가져와짐;;;
-    var qlno = Math.floor(Math.random() * 12) + 1;
+    var qlno = Math.floor(Math.random() * quizSize) + 1;
     location.href = './quiz_view?qlno='+qlno+'&index=0';
 }
